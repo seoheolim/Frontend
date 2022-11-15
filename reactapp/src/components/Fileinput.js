3e8dd496bb333abf570fe356651b29ed6bba0f8a
hyunduk <hyundukseo@naver.com>
2022-11-15 18:37:48 +0900

    [서현덕] 웹 파일 업로드 구현 완료

--- reactapp/src/components/Fileinput.js
+++ reactapp/src/components/Fileinput.js
@@ -1,45 +1,39 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './main.css'
 import axios from "axios"
 
-class Fileinput extends Component {
-
-    constructor(props){
-        super(props);
-        this.state = {
-          selectedFile: null,
-        }
-      }
-
-    handleFileInput(e){
-        this.setState({
-          selectedFile : e.target.files[0],
-        })
-        console.log(e.target.files[0])
-      }
-
-    handlePost(){
-        const formData = new FormData();
-        formData.append('file', this.state.selectedFile);
-    
-        return axios.post("./", formData).then(res => {
-          alert('성공')
-        }).catch(err => {
-          alert('실패')
-        })
-      }
-
-    render() {
-        return (
-            <div className="filebox">
-                <input class="upload-name" value="첨부파일" placeholder="첨부파일"></input>
-                <label for="file">파일찾기</label>
-                <input type="file" id="file" onChange={e => this.handleFileInput(e)}></input>
-
-                <button type="button" onClick={this.handlePost()}>Upload</button>
-            </div>
-        )
-    }
+function handleFileInput(e, setFile) {
+    setFile(e.target.files[0])
+    console.log(e.target.files[0])
 }
 
-export default Fileinput
+function handlePost(image, video) {
+    const formData = new FormData();
+    formData.append('image', image);
+    formData.append('video', video);
+
+    axios.post("http://3.34.44.236:9000/api/upload",
+        formData,
+        { headers: { "Content-Type": "multipart/form-data" } }
+    ).then(res => {
+        alert('성공')
+    }).catch(err => {
+        alert('Failed')
+    })
+}
+
+function Fileinput() {
+    const [image, setImage] = useState();
+    const [video, setVideo] = useState();
+
+
+    return (
+        <div className="filebox">
+            <p><input type="file" id="file" onChange={e => handleFileInput(e, setImage)} /></p>
+            <p><input type="file" id="file" onChange={e => handleFileInput(e, setVideo)} /></p>
+            <p><button type="button" onClick={() => handlePost(image, video)}>Upload</button></p>
+        </div>
+    );
+}
+
+export default Fileinput;
