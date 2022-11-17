8cbf2332e351a943e6744c6dd899f735532b45b7
hyunduk <hyundukseo@naver.com>
2022-11-17 19:48:26 +0900

    서현덕 웹 업데이트

--- reactapp/src/components/Fileinput.js
+++ reactapp/src/components/Fileinput.js
@@ -8,11 +8,35 @@ function handleFileInput(e, setFile, setFileName) {
     console.log(e.target.files[0])
 }
 
-function handlePost(image, video) {
+const handleEmailInput = (e, setEmail) => {
+    setEmail(e.target.value)
+    console.log(e.target.value)
+}
+
+function onlyPerson(e, setStatus) {
+    if (e.target.checked) {
+        setStatus("Only")
+    }
+    else {
+        setStatus("Except")
+    }
+    console.log(e.target.checked)
+}
+function exceptPerson(e, setStatus) {
+    if (e.target.checked) {
+        setStatus("Except")
+    }
+    else {
+        setStatus("Only")
+    }
+    console.log(e.target.checked)
+}
+
+function handlePost(image, video, email) {
     const formData = new FormData();
     formData.append('image', image);
     formData.append('video', video);
-
+    formData.append('email', email);
     axios.post("http://54.180.122.160:9000/api/upload",
         formData,
         { headers: { "Content-Type": "multipart/form-data" } }
@@ -26,22 +50,43 @@ function handlePost(image, video) {
 function Fileinput() {
     const [image, setImage] = useState();
     const [video, setVideo] = useState();
+    const [email, setEmail] = useState();
+    const [status, setStatus] = useState();
+
     const [vidName, setVidName] = useState();
     const [imgName, setImgName] = useState();
 
     return (
         <div className="filebox">
-            <p>
+            <p className='align-center'>
                 <input className="upload-name" placeholder="첨부파일" defaultValue={vidName || ""} />
-                <label htmlFor='vid-file'>동영상 찾기</label>
+                <label htmlFor='vid-file'>영상 찾기</label>
                 <input type="file" id="vid-file" onChange={e => handleFileInput(e, setVideo, setVidName)} />
             </p>
-            <p>
+
+            <p className='align-center'>
                 <input className="upload-name" placeholder="첨부파일" defaultValue={imgName || ""} />
                 <label htmlFor='img-file'>사진 찾기</label>
                 <input type="file" id="img-file" onChange={e => handleFileInput(e, setImage, setImgName)} />
             </p>
-            <p><button type="button" className='btn-submit' onClick={() => handlePost(image, video)}>Upload</button></p>
+
+            <p className='align-center' id='need-space'>
+                대상만 모자이크<input type="checkbox" className='btn-checkbox' onClick={e => onlyPerson(e, setStatus)}></input>
+                <br />
+                대상을 제외하고 모자이크<input type="checkbox" className='btn-checkbox' onClick={e => exceptPerson(e, setStatus)}></input>
+            </p>
+            
+            <h4 className='main-text'>결과물을 받을 이메일을 입력해주세요!</h4>
+            <p className='align-center'>
+                
+                <br />
+                <br />
+
+
+                <input className="email-input" placeholder="E-Mail을 입력해주세요" type="text" onChange={e => handleEmailInput(e, setEmail)}></input>
+            </p>
+
+            <p className='align-center'><button id="btn-submit" type="button" onClick={() => handlePost(image, video, email, status)}>제출하기</button></p>
         </div>
     );
 }
--- reactapp/src/components/main.js
+++ reactapp/src/components/main.js
@@ -4,17 +4,20 @@ import Fileinput from './Fileinput'
 
 class Mainpage extends Component {
 
-    
+
     render() {
         return (
             <div>
                 <h1 className='big-header'>
-                    Upload Your Video!
+                    Upload Your Video and Photo!
                 </h1>
                 <h4 className='main-text'>
-                    모자이크 처리하고 싶은 영상을 올려주세요!
+                    모자이크 처리하고 싶은 영상과 대상의 얼굴 사진을 올려주세요
                 </h4>
-                <Fileinput></Fileinput>
+                <p><br /><Fileinput /></p>
+
+
+
             </div>
         )
     }
